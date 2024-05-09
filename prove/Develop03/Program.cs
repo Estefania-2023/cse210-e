b5de8796711ee46fd5dd926c0665f8bddf5a8bca
Estefania-2023 <estefa20or@gmail.com>
2024-05-09 17:56:38 -0500

    Develop03 finalized

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
+namespace DailyScripture
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+        static void Main()
+        {
+            Console.WriteLine("Welcome to this game!");
+            Console.Write("Put the book's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Put the number of chapter: ");
+            string chapt = Console.ReadLine();
+            int chap =Int32.Parse(chapt);
+            Console.Write("Put the number of verse: ");
+            string vers = Console.ReadLine();
+            int ver =Int32.Parse(vers);
+            Console.Write("Put the verse: ");
+            string text = Console.ReadLine();
+
+            Scripture scripture = new Scripture(name, chap, ver, text);
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nYou got this, you memorized the scripture!!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
     }
 }
